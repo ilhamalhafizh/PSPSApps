ac978d98a77f8f79222ab046b1ff63db610a9d58
ilhamalhafizh <32484628+ilhamalhafizh@users.noreply.github.com>
2019-08-01 12:16:28 +0700

    Add files via upload

---
+++ src/main/java/com/example/pspsapps/MenuActivity.java
@@ -0,0 +1,63 @@
+package com.example.pspsapps;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+//implement the interface OnNavigationItemSelectedListener in your activity class
+public class MenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container,new NewsFragment());
+        fragmentTransaction.commit();
+
+
+        //loading the default fragment
+        loadFragment(new NewsFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_news:
+                fragment = new NewsFragment();
+                break;
+
+            case R.id.navigation_club:
+                fragment = new ClubFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/pspsapps/NewsFragment.java
@@ -0,0 +1,77 @@
+package com.example.pspsapps;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.pspsapps.network.ApiServices;
+import com.example.pspsapps.network.InitRetrofit;
+import com.example.pspsapps.response.BeritaItem;
+import com.example.pspsapps.response.ResponseBerita;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    private RecyclerView recyclerView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_news,container,false);
+        // Inisialisasi Widget
+        recyclerView = (RecyclerView) view.findViewById(R.id.rvListBerita);
+        // RecyclerView harus pakai Layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        // Eksekusi method
+        tampilBerita();
+        return view;
+    }
+    
+    private void tampilBerita() {
+        ApiServices api = InitRetrofit.getInstance();
+        // Siapkan request
+        Call<ResponseBerita> beritaCall = api.request_show_all_berita();
+        // Kirim request
+        beritaCall.enqueue(new Callback<ResponseBerita>() {
+            @Override
+            public void onResponse(Call<ResponseBerita> call, Response<ResponseBerita> response) {
+                // Pasikan response Sukses
+                if (response.isSuccessful()){
+                    Log.d("response api", response.body().toString());
+                    // tampung data response body ke variable
+                    List<BeritaItem> data_berita = response.body().getBerita();
+                    boolean status = response.body().isStatus();
+                    // Kalau response status nya = true
+                    if (status){
+                        // Buat Adapter untuk recycler view
+                        AdapterBerita adapter = new AdapterBerita(getContext(), data_berita);
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        // kalau tidak true
+                        Toast.makeText(getActivity(), "Tidak ada berita untuk saat ini", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBerita> call, Throwable t) {
+                // print ke log jika Error
+                t.printStackTrace();
+            }
+        });
+    }
+    
+}
+
